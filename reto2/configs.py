3faded2b5926e9a372b677a53963fdb71c0b1893
Juanda0 <juandavid.echeverri@mercadolibre.com.co>
2023-08-28 13:24:03 -0500

    grcp server fixes

---
+++ reto2/configs.py
@@ -0,0 +1,3 @@
+GRPC_RULE = '[::]:50051'
+DIRECTORY = './resources'
+GRPC_HOST = '34.234.253.43:50051'
---
+++ reto2/grpc_connection.py
@@ -0,0 +1,19 @@
+import grpc
+from configs import *
+import service_pb2, service_pb2_grpc
+
+class grpc_connection:
+    def __init__(self):
+        self.channel = grpc.insecure_channel(GRPC_HOST)
+        self.client = service_pb2_grpc.fileServiceStub(self.channel)
+
+    def search_product(self, file_name):
+        response = self.client.SearchFiles(service_pb2.File(fileName=file_name))
+        return response
+
+    def list_products(self):
+        files = []
+        for file in self.client.ListAllFiles(service_pb2.Empty()):
+            files.append(file)
+        return files
+
