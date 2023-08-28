d15b420ba3e4a453caad5bb1eda89522f98b8b77
Juanda0 <juandavid.echeverri@mercadolibre.com.co>
2023-08-28 14:18:13 -0500

    return fixes

--- reto2/grpc_server.py
+++ reto2/grpc_server.py
@@ -11,30 +11,28 @@ class FileService(service_pb2_grpc.fileServiceServicer):
       target_string = request.fileName
       print("Searching products that match string: " + target_string)
       matched_files = []
-      for root, _, files in os.walk(DIRECTORY):
-        for file in files:
-            if target_string in file:
-                file_path = os.path.join(root, file)
-                file_size = os.path.getsize(file_path)
-                file_mtime = os.path.getmtime(file_path)
-                matched_files.append(service_pb2.singleFileResponse(name = file, lastUpdated = file_mtime, size = file_size))
-      yield service_pb2.multipleFilesResponse(files=matched_files)
+      for file in os.listdir(DIRECTORY):
+        if target_string in file:
+            file_path = os.path.join(DIRECTORY, file)
+            file_size = os.path.getsize(file_path)
+            file_mtime = os.path.getmtime(file_path)
+            file_mtime_datetime = datetime.datetime.fromtimestamp(file_mtime)
+            formatted_mtime = file_mtime_datetime.strftime("%Y-%m-%d %H:%M:%S")
+            matched_files.append(service_pb2.singleFileResponse(name = file, lastUpdated = formatted_mtime, size = file_size))
+      return service_pb2.multipleFilesResponse(files=matched_files)
 
    def ListAllFiles(self, request, context):
       print("Listing all files")
       all_files_info = []
       for file in os.listdir(DIRECTORY):
-            print(file)
             file_path = os.path.join(DIRECTORY, file)
             file_size = os.path.getsize(file_path)
             file_mtime = os.path.getmtime(file_path)
-            print(1)
             file_mtime_datetime = datetime.datetime.fromtimestamp(file_mtime)
             formatted_mtime = file_mtime_datetime.strftime("%Y-%m-%d %H:%M:%S")
-            print(file, formatted_mtime, file_size)
             all_files_info.append(service_pb2.singleFileResponse(name = file, lastUpdated = formatted_mtime, size = file_size))
       print(all_files_info)
-      yield service_pb2.multipleFilesResponse(files=all_files_info)
+      return service_pb2.multipleFilesResponse(files=all_files_info)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
